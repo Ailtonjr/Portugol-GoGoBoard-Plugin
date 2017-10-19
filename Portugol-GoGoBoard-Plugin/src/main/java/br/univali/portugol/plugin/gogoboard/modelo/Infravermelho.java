cb38348758fe40901f292d13ed4ddfddb9104207
Ailton Cardoso Júnior <ailtoncardoso0@gmail.com>
2017-10-19 04:08:24 -0200

    Adicionado classe infravermelho

---
+++ Portugol-GoGoBoard-Plugin/src/main/java/br/univali/portugol/plugin/gogoboard/modelo/Infravermelho.java
@@ -0,0 +1,17 @@
+package br.univali.portugol.plugin.gogoboard.modelo;
+
+/**
+ *
+ * @author Ailton Cardoso Jr
+ */
+public class Infravermelho {
+    private int valorRecebido;
+
+    public int getValorRecebido() {
+        return valorRecebido;
+    }
+
+    public void setValorRecebido(int valorRecebido) {
+        this.valorRecebido = valorRecebido;
+    }
+}
